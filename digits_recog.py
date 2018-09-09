62038028e87e9ebc3b3fc13f49e8b78c9341ba7e
skazi019 <skazi019@gmail.com>
2018-09-09 19:57:01 +0530

    Trying to apply svm

--- digits_recog.py
+++ digits_recog.py
@@ -10,6 +10,15 @@ train.shape
 test.shape
 
 import matplotlib.pyplot as plt
+# Selecting 4th row/number and all columns except Label
 image = np.array(train.iloc[3,1:], dtype='float')
 image = image.reshape((28,28))
 plt.imshow(image, cmap='gray')
+
+from sklearn.model_selection import train_test_split
+X = train.iloc[:,1:]
+y = train.iloc[:,0]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+from sklearn import svm
+model = svm.SVC()
+model.fit(X_train, y_train)
