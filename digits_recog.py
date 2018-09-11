bfa89aeb33a45c64d9506288a75ed02e0115c323
skazi019 <skazi019@gmail.com>
2018-09-11 20:25:30 +0530

    Predicting using DecisionTreeClassifier and Checking the output of the test dataset

--- digits_recog.py
+++ digits_recog.py
@@ -21,9 +21,22 @@ y = train.iloc[:,0]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 from sklearn.tree import DecisionTreeClassifier
-dtc = DecisionTreeClassifier()
+dtc = DecisionTreeClassifier(max_depth=14)
 dtc.fit(X_train, y_train)
 
 from sklearn.metrics import accuracy_score
 print('Accuracy Score : ', accuracy_score(y_train, dtc.predict(X_train)))
 print('Accuracy Score : ', accuracy_score(y_test, dtc.predict(X_test)))
+
+test['label'] = dtc.predict(test)
+test.head()
+img = np.array(test.iloc[3,:-1],dtype='float')
+img = img.reshape((28,28))
+plt.title(test.iloc[3,-1])
+plt.imshow(img, cmap='gray')
+
+test = test['label']
+test.head()
+test.shape
+test.columns = ['ImageId', 'Label']
+test.to_csv('res_dtc.csv')
